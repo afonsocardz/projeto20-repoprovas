635d0230f757636e56c98bb3838fe69b0bca2fcc
afonsocardz <afonso.desenvolvimento@gmail.com>
2022-09-19 14:34:59 -0300

    fix teachers migration with @@map

---
+++ prisma/migrations/20220919173403_fix_teachers_table_with_map/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Teacher` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Teacher";
+
+-- CreateTable
+CREATE TABLE "teachers" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "teachers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teachers_name_key" ON "teachers"("name");
