125fdcbd2beccdbd9a99578eeba9d902ac67d562
afonsocardz <afonso.desenvolvimento@gmail.com>
2022-09-19 14:19:19 -0300

    feat: add validate token router after user router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
+import { validateToken } from '../middlewares/validateTokenMiddleware';
 import userRouter from './userRouters';
 
 const router = Router();
 
 router.use(userRouter);
+router.use(validateToken);
 
 export default router;
